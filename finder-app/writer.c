30b64e104c590925e1f53d37066547e8862830fa
HasanBasriBILGE <hasan.bilge1453@gmail.com>
2023-08-26 14:48:23 +0300

    Attempt

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,41 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <syslog.h>
+#include <stdlib.h>
+#include <sys/syslog.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    // Check if both arguments are provided
-    if (argc != 3) {
-        printf("Usage: %s <writefile> <writestr>\n", argv[0]);
-        exit(1);
+
+int main(int argc, char* argv[]){
+
+    openlog("WriterApp", LOG_NDELAY, LOG_LOCAL0);
+
+
+    // check argument first
+    if (argc != 3){
+        syslog(LOG_ERR, "App needs exactly two arguments");
+        return 1;
     }
 
-    const char *writefile = argv[1];
-    const char *writestr = argv[2];
+    // store arguments
+    const char* file_name = argv[1];
+    const char* content = argv[2];
 
-    // Open the file for writing
-    FILE *file = fopen(writefile, "w");
-    if (file == NULL) {
-        syslog(LOG_ERR, "Error: Could not open file '%s' for writing", writefile);
-        exit(1);
+    int fd = open(file_name, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+    if (fd == -1){
+        syslog(LOG_ERR, "Cannot open the given file");
+        return 1;
     }
 
-    // Write the content to the file
-    if (fprintf(file, "%s", writestr) < 0) {
-        syslog(LOG_ERR, "Error: Could not write to file '%s'", writefile);
-        fclose(file);
-        exit(1);
+    int return_val = write(fd, content, strlen(content));
+
+    if (return_val == -1){
+        syslog(LOG_ERR, "Couldnt complete the write operation, make sure you have appropriate permissions");
+        return 1;
     }
 
-    // Close the file
-    fclose(file);
+    syslog(LOG_INFO, "Successfully wrote the content to file!");
 
-    // Log the write operation
-    syslog(LOG_DEBUG, "Writing '%s' to '%s'", writestr, writefile);
 
-    // Print a success message
-    printf("File '%s' created with content:\n%s\n", writefile, writestr);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
-
